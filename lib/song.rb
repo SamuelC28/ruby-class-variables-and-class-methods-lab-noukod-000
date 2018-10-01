3c222c618f498057f946ea499161e777a4eeb23d
Samuel Cornet <samuel@noukod.com>
2018-10-01 18:01:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,9 +29,15 @@ class Song
       def self.artists
         @@artists.uniq
       end
-        def self.genre_count
+      def self.genre_count
         genre_count = Hash.new(0)
         @@genres.each { |genre| genre_count[genre] +=1  }
+        return genre_count
+      end
+
+      def self.artist_count
+        artist_count = Hash.new(0)
+        @@artists.each { |artist| artist_count[artist] +=1  }
         return genre_count
       end
 
