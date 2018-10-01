6fd49b104791e38dbd100d3ad44984a0de290a4b
Samuel Cornet <samuel@noukod.com>
2018-10-01 17:32:03 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song
+  attr_accessor :name, :artist, :genre
+      @@count = 0
+      @@artists = []
+      @@genres = []
+      @@genre_count = {}
+
+
+
+   def initialize(name, artist, genre)
+       @name = name
+       @genre = genre
+       @artist = artist
+
+       @@artists << artist
+       @@genres << genre
+       @@count += 1
+       @@genre_count
+
+   end
+
+
+      def self.count
+        @@count
+      end
+      def self.genre
+        @@genre.uniq
+      end
+      def self.artists
+        @@artists.uniq
+      end
+      def self.genre_count
+        @@genre_count
+      end
+
+
+end
